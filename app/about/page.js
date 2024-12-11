c0a77c2ff9f61ac8d05dbf52fda82aea1233e3a2
Yassine Ghannay <yassineghannay13@gmail.com>
2024-12-10 18:10:50 -0700

    commit23

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import Navbar from '../components/Navbar';
 
 export default function About() {
@@ -15,14 +16,14 @@ export default function About() {
       link: "https://github.com/yassineghannay/PyLivestream",
     },
     {
-        title: "MonsterMakerC",
-        description: "Simple C# program where you can create your own monster!!.",
-        link: "https://github.com/yassineghannay/MonsterMakerC",
+      title: "MonsterMakerC",
+      description: "Simple C# program where you can create your own monster!!.",
+      link: "https://github.com/yassineghannay/MonsterMakerC",
     },
     {
-        title: "MortgageAndBanK",
-        description: "Mortgage Calculator using simple python concepts.",
-        link: "https://github.com/yassineghannay/MortgageAndBankV6",
+      title: "MortgageAndBanK",
+      description: "Mortgage Calculator using simple Python concepts.",
+      link: "https://github.com/yassineghannay/MortgageAndBankV6",
     },
   ];
 
@@ -32,12 +33,21 @@ export default function About() {
       <main className="max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-lg mt-6">
         <h1 className="text-3xl font-bold text-blue-600">About Me</h1>
         <section className="mt-4 text-gray-700">
-          <p>
-            Hello! My name is Yassine Ghannay, a second-year Software Development student at
-            Southern Alberta Institute of Technology. I'm passionate about coding and have skills
-            in various technologies like JavaScript, Python, C#, and more. I enjoy working on
-            projects that challenge me to grow and expand my knowledge.
-          </p>
+          <div className="flex flex-col md:flex-row items-center gap-8">
+            <Image
+              src="/WIN_20241210_17_53_11_Pro.jpg"
+              alt="Picture of Yassine Ghannay"
+              width={200}
+              height={200}
+              className="rounded-full shadow-lg"
+            />
+            <p className="text-lg">
+              Hello! My name is Yassine Ghannay, a second-year Software Development student at
+              Southern Alberta Institute of Technology. I'm passionate about coding and have skills
+              in various technologies like JavaScript, Python, C#, and more. I enjoy working on
+              projects that challenge me to grow and expand my knowledge.
+            </p>
+          </div>
         </section>
 
         <section className="mt-8">
